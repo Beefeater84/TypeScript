44c723f50fa4b3e9bb321d2645a1051374160b22
Beefeater84 <site2max@gmail.com>
2021-04-16 19:44:43 +0300

    app.hs from main file to Router

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,53 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
+// Для установки библиотек в TS испольузем @types = npm i @types/react-router-dom
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import {Navbar} from "./components/Navbar";
-import {ToDoFormRef} from "./components/ToDoFormRef";
-import {ToDoList} from "./components/ToDoList";
-// import {ToDoForm} from "./components/ToDoForm";
-import {ITodo} from "./interfaces"
+import {TodoPages} from "./pages/TodoPages";
+import {AboutPage} from "./pages/AboutPage";
 
 
 const App: React.FC = () => {
 
-    const [todos, setTodos] = useState<ITodo[]>([])
-
-    const todoHandler = (title: string): void => {
-        const newToDo: ITodo = {
-            title: title,
-            id: Date.now(),
-            completed: false
-        }
-
-        // Можно обновлять state так, но он обновляется асинхронно, поэтому более правильная запись обновлять, основываясь на предыдущем стейте
-        // setTodos([newToDo, ...todos])
-
-        setTodos(prew => [newToDo, ...prew])
-    }
-
-    const RemoveHandler = (id: number) => {
-        setTodos(prevState => prevState.filter(todo => todo.id !== id))
-    }
-
-    const ToggleHandler = (id: number) => {
-        const newTodos = todos.map(todo => {
-            if (todo.id === id)
-            todo.completed = !todo.completed
-            return todo
-        })
-        setTodos(newTodos)
-    }
-
-    // В React Ts нужно указывать какие именно Props мы ждем, поэтому в ToDoFormRef указываем addToDo, иначе этот проект не соберется
-
     return (
-        <>
+        <BrowserRouter>
             <Navbar/>
             <div className="container">
-                <ToDoFormRef addTodo={todoHandler}/>
-                <ToDoList todos={todos} onToggle={ToggleHandler} onRemove={RemoveHandler}/>
+                <Switch>
+                    <Route component={TodoPages} path="/" exact/>
+                    <Route component={AboutPage} path="/about"/>
+                </Switch>
             </div>
-
-        </>
-
+        </BrowserRouter>
     );
 }
 
